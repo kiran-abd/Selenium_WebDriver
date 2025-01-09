eda119116453b5330eef4144c112173f2eaf2c95
kiran-abd <kiran.abd93@gmail.com>
2025-01-09 19:55:00 +0530

    second Commit

---
+++ src/test/Examples.java
@@ -0,0 +1,29 @@
+package test;
+
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Examples {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+ System.setProperty("WebDriver.Chrome.Driver", "chromedriver.exe");
+ WebDriver driver = new ChromeDriver();
+		
+		
+			
+			
+			
+			
+		
+	}
+
+}
